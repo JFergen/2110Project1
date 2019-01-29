e0f50baa93aa582b484232853375fa9d1f052504
Joseph Fergen <josephfergen150@gmail.com>
2019-01-29 12:20:10 -0600

    Test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    cout << "This is a test" << endl;
     return 0;
 }
