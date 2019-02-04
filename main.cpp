e8a66142dad59e922b9cc11051394a40c6e32d81
Joseph Fergen <josephfergen150@gmail.com>
2019-02-03 23:42:44 -0600

    Test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
-    cout << "This is a test" << endl;
+    string foodItems; // Should be a list separated by commas
+    vector<string> foodList; // Vector of all different food items
 
-    cout << "Testing over here now" << endl;
+    cin >> foodItems; // Should be a list separated by commas
+
+    istringstream foodRead(foodItems); // Separating commas from list
+    string parseHelp; // Used to help with parsing through list
+
+    while (getline(foodRead, parseHelp, ',')) // Parses through the comma'd list and sets each food item to be an element in vector
+    {
+        foodList.push_back(parseHelp);
+    }
+
+    for (int i = 0; i < foodList.size(); i++)
+    {
+        cout << "Food #" << i + 1 << foodList.at(i) << endl;
+    }
+
     return 0;
 }
